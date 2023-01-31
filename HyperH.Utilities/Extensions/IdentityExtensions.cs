6be5d763d175e11e1bd6d195d0c8e66654679f26
hashem <hashem.1990@live.com>
2023-01-31 23:40:25 +0100

    Add Identity GetUserId extension

---
+++ HyperH.Utilities/Extensions/IdentityExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Security.Claims;
+using System.Security.Principal;
+
+
+namespace HyperH.Utilities.Extensions
+{
+	public class IdentityExtensions
+	{
+		public static string GetUserId(ClaimsPrincipal identity)
+		{
+			return identity.FindFirstValue(ClaimTypes.NameIdentifier);
+        }
+	}
+}
+
